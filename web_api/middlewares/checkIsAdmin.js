db0868f68633492d04a9367dd9ae107786227125
smaksimenko <smaksimenko@lohika.com>
2019-12-16 10:58:27 +0200

    update

--- web_api/middlewares/checkIsAdmin.js
+++ web_api/middlewares/checkIsAdmin.js
@@ -1,5 +1,5 @@
 module.exports = function isAdmin (req, res, next) {
-  if (req.user.role === 'ADMIN') {
+  if (req.user.role !== 'ADMIN') {
     const err = new Error('Authorisation error');
     err.status = 505;
     next(err);
